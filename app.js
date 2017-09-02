a818eb7b3003a3d49270cebf29f38bffc42582e8
Franck Aragão <fjunior.aragao@gmail.com>
2017-09-01 23:29:10 -0300

    pegando do dist

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3400));
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
-    res.sendFile(__dirname + '/src/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 });
 
 var server = app.listen(app.get('port'), function() {
