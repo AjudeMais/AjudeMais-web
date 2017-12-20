321f0da48370ab128d86fd6c61ae6987c94365af
Franck Aragão <fjunior.aragao@gmail.com>
2017-12-19 22:50:45 -0300

    manifest  e sw

--- sw.js
+++ sw.js
@@ -5,14 +5,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
         '/',
-        '/app',
-        '/app/js',
-        '/content',
-        '/content/css',
-        '/index.html',
-        '/app/js/plugins.min.js',
-        '/app/js/scripts.min.js',
-        '/content/css/main.css'
+        'app',
+        'app/js',
+        'content',
+        'content/css',
+        'index.html',
+        'app/js/plugins.min.js',
+        'app/js/scripts.min.js',
+        'content/css/main.css'
       ]);
     })
   )
